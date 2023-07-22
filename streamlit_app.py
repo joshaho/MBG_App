17739280d88ce917bdbbfc38b50dfa3a01779a6e
Josha Ho <josha170@gmail.com>
2023-07-22 12:56:37 -0400

    try writing wrong

--- streamlit_app.py
+++ streamlit_app.py
@@ -320,7 +320,7 @@ class setup():
                 st.write('Done Course Table')
                 data.setTable(course_offering_table, course_offering_write_table, offering_id, timestamp_int)
                 st.write('Done Course Offering')
-                data.setTable(student_table, student_write_table, offering_id, timestamp_int) #setTable(tablename,table, partition, row_index=0)
+                data.setTable(student_table, course_offering_write_table, offering_id, timestamp_int) #setTable(tablename,table, partition, row_index=0)
 
                 st.write('Done Student Table')
                 st.write("Saved!")
