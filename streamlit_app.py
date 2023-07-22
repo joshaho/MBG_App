7b1b5b8cea2309994e0e72cc78aa5912c6373a3e
Josha Ho <josha170@gmail.com>
2023-07-22 13:30:20 -0400

    try cache fn

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,18 +204,27 @@ class data():
             return option
 
 
+def cache():
+    students = pd.DataFrame(data.getTab('contacts'))[4]
+    student_ids =[10000, 10001, 10002]
+    targets = ["L.1", "L.2"]
+    course_offering = ["MAT230-F2022-01"]
+
 # In[7] Global Filters to Call Back:
 def meeting_filters(inline=True):
+
+    
+
     if inline==True:
-        course_offering = st.selectbox("Course Offering", data.course_offering)
+        course_offering = st.selectbox("Course Offering", cache.course_offering)
         date = st.date_input("Meeting Date")
-        student = st.selectbox("Student Name", data.students)
-        learning_target = st.selectbox("Learning Target", data.targets)
+        student = st.selectbox("Student Name", cache.students)
+        learning_target = st.selectbox("Learning Target", cache.targets)
     else:
-        course_offering = st.sidebar.selectbox("Course Offering", data.add_all(data.course_offering))
+        course_offering = st.sidebar.selectbox("Course Offering", cache.add_all(data.course_offering))
         date = st.sidebar.date_input("Meeting Date")
-        student = st.sidebar.selectbox("Student Name", data.add_all(data.students))
-        learning_target = st.sidebar.selectbox("Learning Target", data.add_all(data.targets))
+        student = st.sidebar.selectbox("Student Name", data.add_all(cache.students))
+        learning_target = st.sidebar.selectbox("Learning Target", cache.add_all(data.targets))
     return course_offering, date, student, learning_target
 
 # In[6] Meeting Logger App:
